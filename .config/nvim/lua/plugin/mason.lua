ecf144662c494f69804a164fd33f22b567df83b2
Jakob Jensen <jakobj@jakobj.dk>
2025-10-15 18:03:15 +0200

    tmp

--- .config/nvim/lua/plugin/mason.lua
+++ .config/nvim/lua/plugin/mason.lua
@@ -1,7 +1,15 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    { 'mason-org/mason.nvim', opts = {} },
+    {
+      'mason-org/mason.nvim',
+      opts = {
+        registries = {
+          'github:mason-org/mason-registry',
+          'github:Crashdummyy/mason-registry',
+        },
+      },
+    },
     'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
