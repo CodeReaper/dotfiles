26ff1070fc771921288b8a574dce33d55b0758b9
Jakob Jensen <jakobj@jakobj.dk>
2025-10-15 16:26:39 +0200

    tmp

--- .config/nvim/lua/plugin/blink.lua
+++ .config/nvim/lua/plugin/blink.lua
@@ -5,5 +5,16 @@ return {
       preset = 'enter',
     },
     fuzzy = { implementation = 'lua' },
+    completion = {
+      documentation = {
+        auto_show = true,
+      },
+      ghost_text = {
+        enabled = true,
+      },
+    },
+    signature = {
+      enabled = true,
+    },
   },
 }
